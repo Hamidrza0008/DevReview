5f2f758ede86d8f617185dfb92b447511ab09d8a
hamidrza0008 <hamidrza0008@gmail.com>
2026-08-10 01:34:46 +0530

    Started Building  V2 and added notification model and schema

---
+++ backend/models/Notification.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+const Users = require("./Users");
+
+const notificationSchema = new mongoose.Schema({
+    recipient:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Users",
+        required:true
+    },
+    sender:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Users",
+        required:true,
+    },
+    type:{
+        type:String,
+        enum:["like" , "review", "follow"],
+        required:true,
+    },
+    project: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Project",
+      default: null,
+    },
+
+    isRead: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+)
+
+const Notification  = mongoose.model("Notification" , notificationSchema);
+module.exports = Notification;
